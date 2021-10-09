028a3f723a91117908fe4fa77a91893ad3e9e88b
Connor Van Ooyen <connor.vanooyen@gmail.com>
2021-10-08 23:03:54 -0600

    removed duplicate ciks

--- main/sec_filings/sec_filings.py
+++ main/sec_filings/sec_filings.py
@@ -9,4 +9,12 @@ def get_filings_between_dates(start_date: str, end_date: str) -> List[PartialFil
     filings: List[PartialFiling] = get_ex991(start_date, end_date, [])
     final_filings: List[PartialFiling] = get_data_from_filing(filings)
 
-    return final_filings
+    # remove duplicate companies
+    used_ciks = []
+    unique_filings = []
+    for filing in final_filings:
+        if filing.cik not in used_ciks:
+            used_ciks.append(filing.cik)
+            unique_filings.append(filing)
+
+    return unique_filings
